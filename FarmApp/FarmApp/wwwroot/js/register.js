de47b0807b9f27e1f99b165f2756840b3e7e36ac
castic96 <zdenek.castoral.jr@gmail.com>
2021-04-11 13:16:20 +0200

    Rozdělení registrace - customer, farmer.

--- FarmApp/FarmApp/wwwroot/js/register.js
+++ FarmApp/FarmApp/wwwroot/js/register.js
@@ -1,18 +1,9 @@
 ﻿$(function () {
-
-	$('#customer-form-link').click(function (e) {
-		$("#customer-form").delay(100).fadeIn(100);
-		$("#farmer-form").fadeOut(100);
-		$('#farmer-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#farmer-form-link').click(function (e) {
-		$("#farmer-form").delay(100).fadeIn(100);
-		$("#customer-form").fadeOut(100);
-		$("#customer-form-link").removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
-});
+    var current = this.location.pathname;
+    $('.nav-tabs li a').each(function () {
+        var $this = $(this);
+        if (current.toLowerCase().valueOf() == $this.attr('href').toLowerCase().valueOf()) {
+            $this.addClass('active');
+        }
+    })
+})
