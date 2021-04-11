bdd252c0a209bdcb47c25e4784dc47ab491ae571
castic96 <zdenek.castoral.jr@gmail.com>
2021-04-11 12:23:40 +0200

    Úprava registrace.

--- FarmApp/FarmApp/wwwroot/js/register.js
+++ FarmApp/FarmApp/wwwroot/js/register.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(function () {
+
+	$('#customer-form-link').click(function (e) {
+		$("#customer-form").delay(100).fadeIn(100);
+		$("#farmer-form").fadeOut(100);
+		$('#farmer-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#farmer-form-link').click(function (e) {
+		$("#farmer-form").delay(100).fadeIn(100);
+		$("#customer-form").fadeOut(100);
+		$("#customer-form-link").removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
