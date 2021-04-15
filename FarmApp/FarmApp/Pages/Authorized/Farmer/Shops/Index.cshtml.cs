07498c53e439ec6c5866dd1b3a26cfdc28c8b4b5
castic96 <zdenek.castoral.jr@gmail.com>
2021-04-15 13:24:04 +0200

    Načtení pouze těch shopů, které uživatel vlastní.

--- FarmApp/FarmApp/Pages/Authorized/Farmer/Shops/Index.cshtml.cs
+++ FarmApp/FarmApp/Pages/Authorized/Farmer/Shops/Index.cshtml.cs
@@ -7,23 +7,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using FarmApp.Data;
 using FarmApp.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace FarmApp.Pages.Authorized.Farmer.Shops
 {
     public class IndexModel : PageModel
     {
         private readonly FarmApp.Data.FarmAppContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public IndexModel(FarmApp.Data.FarmAppContext context)
+        public IndexModel(FarmApp.Data.FarmAppContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<Shop> Shop { get;set; }
 
         public async Task OnGetAsync()
         {
-            Shop = await _context.Shops.ToListAsync();
+            Shop = await _context.Shops.Where(shop => shop.Owner.Id == _userManager.GetUserId(User)).ToListAsync();
         }
     }
 }
