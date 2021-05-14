9ef2dab5d7bdd13b917c540a8357a265523c59d8
castic96 <zdenek.castoral.jr@gmail.com>
2021-05-14 14:06:27 +0200

    Inicializace mapy.

---
+++ FarmApp/FarmApp/Pages/Authorized/Customer/Map.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FarmApp.Data;
+using FarmApp.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace FarmApp.Pages.Authorized.Customer.Find
+{
+    public class MapModel : PageModel
+    {
+        private readonly FarmApp.Data.FarmAppContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public MapModel(FarmApp.Data.FarmAppContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IList<Shop> Shop { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Shop = await _context.Shops
+                                    .OrderByDescending(shop => shop.CreateDate)
+                                    .ToListAsync();
+        }
+    }
+}
