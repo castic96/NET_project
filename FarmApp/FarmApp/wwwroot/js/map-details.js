0861574b9c2f601cfa3eded808eced64206793a8
castic96 <zdenek.castoral.jr@gmail.com>
2021-05-18 15:56:38 +0200

    Náhled mapy v detailech.

---
+++ FarmApp/FarmApp/wwwroot/js/map-details.js
@@ -0,0 +1,20 @@
+﻿let map;
+let markers = [];
+
+function initMap() {
+    let latitude = Number(document.getElementById('latitude').value);
+    let longitude = Number(document.getElementById('longitude').value);
+    let position = { lat: latitude, lng: longitude };
+
+
+    map = new google.maps.Map(document.getElementById("map-details"), {
+        zoom: 12,
+        center: position
+    });
+
+    new google.maps.Marker({
+        position: position,
+        map: map,
+    });
+
+}
