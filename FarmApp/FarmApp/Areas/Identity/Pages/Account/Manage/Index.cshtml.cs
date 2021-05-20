2558895f1b5542b909cf7683ac48b7217ba86cb3
castic96 <zdenek.castoral.jr@gmail.com>
2021-05-20 12:29:02 +0200

    Editování osobních dat.

--- FarmApp/FarmApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ FarmApp/FarmApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -7,6 +7,7 @@ using FarmApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FarmApp.Areas.Identity.Pages.Account.Manage
 {
@@ -14,41 +15,55 @@ namespace FarmApp.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly FarmApp.Data.FarmAppContext _context;
 
         public IndexModel(
             UserManager<User> userManager,
-            SignInManager<User> signInManager)
+            SignInManager<User> signInManager,
+            FarmApp.Data.FarmAppContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
         }
-
-        public string Username { get; set; }
-
-        [TempData]
-        public string StatusMessage { get; set; }
 
         [BindProperty]
         public InputModel Input { get; set; }
 
+        [TempData]
+        public string StatusMessage { get; set; }
+
         public class InputModel
         {
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
-        }
+            [Required]
+            [DataType(DataType.EmailAddress)]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
 
-        private async Task LoadAsync(User user)
-        {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
 
-            Username = userName;
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
 
-            Input = new InputModel
-            {
-                PhoneNumber = phoneNumber
-            };
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Street")]
+            public string Street { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "City")]
+            public string City { get; set; }
+
+            [Required]
+            [DataType(DataType.PostalCode)]
+            [Display(Name = "Postal Code")]
+            public int PostalCode { get; set; }
         }
 
         public async Task<IActionResult> OnGetAsync()
@@ -59,13 +74,24 @@ namespace FarmApp.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            await LoadAsync(user);
+            var userEntity = await _context.Users.FirstOrDefaultAsync(u => u.Id.Equals(_userManager.GetUserId(User)));
+
+            Input = new InputModel();
+
+            Input.Email = userEntity.Email;
+            Input.FirstName = userEntity.FirstName;
+            Input.LastName = userEntity.LastName;
+            Input.Street = userEntity.Street;
+            Input.City = userEntity.City;
+            Input.PostalCode = userEntity.PostalCode;
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
             var user = await _userManager.GetUserAsync(User);
+            
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
@@ -73,22 +99,22 @@ namespace FarmApp.Areas.Identity.Pages.Account.Manage
 
             if (!ModelState.IsValid)
             {
-                await LoadAsync(user);
                 return Page();
             }
 
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
-                }
-            }
+            var userEntity = await _context.Users.FirstOrDefaultAsync(u => u.Id.Equals(_userManager.GetUserId(User)));
 
+            userEntity.FirstName = Input.FirstName;
+            userEntity.LastName = Input.LastName;
+            userEntity.Street = Input.Street;
+            userEntity.City = Input.City;
+            userEntity.PostalCode = Input.PostalCode;
+
+            _context.Users.Update(userEntity);
+
+            await _context.SaveChangesAsync();
             await _signInManager.RefreshSignInAsync(user);
+
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
         }
